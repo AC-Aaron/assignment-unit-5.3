c8d4e5910ff415b1515c387e4ade9230635d08b7
Aaron Aguilar-Chavez <oyorom8@gmail.com>
2023-02-14 14:45:21 -0600

    created var collection, added addtocollection function, added 2 albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+    const newAlbum = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+
+    
+    }
+    collection.push(newAlbum)
+    return newAlbum
+}
+
+console.log(addToCollection('Nevermind', 'Nirvana', 1991));
+console.log(addToCollection('ATLiens', 'Outkast', 1996));

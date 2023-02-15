8bb1816b4009be90f6d337ba2d417aff1310f0d9
Aaron Aguilar-Chavez <oyorom8@gmail.com>
2023-02-15 09:07:27 -0600

    started showCollection, test failed

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,8 +2,10 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+console.log('***addToCollection***');
+
 function addToCollection(title, artist, yearPublished){ //params for object to create.
-    const newAlbum = {
+    const newAlbum = {  //create new object
         title: title,
         artist: artist,
         yearPublished: yearPublished
@@ -20,5 +22,19 @@ console.log(addToCollection('Testing', 'ASAP Rocky', 2018));
 console.log(addToCollection('Scorpion', 'Drake', 2018));
 console.log(addToCollection('Thank Me Later', 'Drake', 2010));
 console.log(addToCollection('Greatest Hits','Tupac Shakur', 1998));
+//end +6 albums
 
-console.log(collection);
+console.log(collection);    //log collection array
+
+console.log('***showCollection***');
+
+function showCollection(){
+    //x = collection.x
+    for (info of collection){
+        console.log(collection.title +" by "+ collection.artist + 
+                    " published in " + collection.yearPublished );
+    }
+    
+}
+
+console.log(showCollection);

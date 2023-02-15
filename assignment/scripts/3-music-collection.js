0d097e72ffc36041f639f27c3e63336899a1660c
Aaron Aguilar-Chavez <oyorom8@gmail.com>
2023-02-15 08:50:22 -0600

    +6 albums, logged collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,17 +2,23 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection(title, artist, yearPublished){
+function addToCollection(title, artist, yearPublished){ //params for object to create.
     const newAlbum = {
         title: title,
         artist: artist,
         yearPublished: yearPublished
 
     
-    }
-    collection.push(newAlbum)
-    return newAlbum
+    }   //end create newAlbum object. 
+    collection.push(newAlbum)   //adding newAlbum to the collection array
+    return newAlbum             //end
 }
 
 console.log(addToCollection('Nevermind', 'Nirvana', 1991));
 console.log(addToCollection('ATLiens', 'Outkast', 1996));
+console.log(addToCollection('Testing', 'ASAP Rocky', 2018));
+console.log(addToCollection('Scorpion', 'Drake', 2018));
+console.log(addToCollection('Thank Me Later', 'Drake', 2010));
+console.log(addToCollection('Greatest Hits','Tupac Shakur', 1998));
+
+console.log(collection);
